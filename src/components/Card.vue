f104a8b2204320924addbe91c413b1c42054cc61
Justin Renjilian <justin.renjilian@gmail.com>
2020-03-23 20:43:49 -0400

    Lots of card styling.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,51 +1,115 @@
 <template>
-  <div :class="[$style.card, $style[card.suit]]">
+  <label
+    :for="card.id"
+    :class="[
+      $style.card,
+      $style[card.suit],
+      selected ? $style.selected : ''
+    ]"
+  >
+    <input
+      :id="card.id"
+      ref="checkbox"
+      type="checkbox"
+      :class="$style.checkbox"
+      @change="toggle"
+    />
     <p :class="[$style.name, $style.top]">{{ card.name }}</p>
     <p :class="[$style.name, $style.bottom]">{{ card.name }}</p>
-  </div>
+  </label>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
-import { Card } from '../logic/card';
+import { defineComponent, ref } from "@vue/composition-api";
+import { Card } from "../logic/card";
 
 export default defineComponent({
-  name: 'Card',
+  name: "Card",
   props: {
-    card: { type: Object as () => Card, required: true },
+    card: { type: Object as () => Card, required: true }
   },
+  setup: () => {
+    const selected = ref(false);
+    const checkbox = ref<HTMLInputElement>(null);
+
+    const toggle = () => {
+      if (checkbox.value) {
+        selected.value = checkbox.value.checked;
+      }
+    };
+
+    return {
+      checkbox,
+      selected,
+      toggle,
+    };
+  }
 });
 </script>
 
 <style lang="less" module>
+.container {
+  position: relative;
+}
+
+.checkbox {
+  opacity: 0;
+}
+
+.checkbox:checked + .card {
+  transform: scale(1.3);
+}
+
 .card {
+  --c-card: #000;
+  --shadow-color: #ddd;
+
   display: block;
-  border-radius: 5px;
   height: 150px;
   width: 100px;
   position: relative;
+  top: 0;
+
+  transform: scale(1);
+  will-change: transform;
+  transition: transform 200ms;
 
   border: 1px solid #eee;
-  box-shadow: 2px 2px 6px 0 #ddd;
+  border-radius: 5px;
+  box-shadow: 2px 2px 6px 0 var(--shadow-color);
+  background-color: #fff;
+  color: var(--c-card);
+
+  &:hover {
+    transform: scale(1.15);
+  }
+
+  &.selected {
+    transform: scale(1.3);
+  }
+  
+  &:focus-within {
+    --shadow-color: var(--c-card);
+  }
 
   &.red {
-    color: #ff2f00;
+    --c-card: #ff2f00;
   }
 
   &.blue {
-    color: #005bff;
+    --c-card: #005bff;
   }
 
   &.green {
-    color: #00b82e;
+    --c-card: #00b82e;
   }
 
   &.black {
-    color: #444444;
+    --c-card: #444444;
   }
 
   &.special {
-    color: #efc940;
+    --c-card: #efc940;
   }
 }
 
